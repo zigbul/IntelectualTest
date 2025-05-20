a5de702a3964e1d4db0a0e4bd259e566c41b89cc
Zigbul <parfenov.georgiy@bk.ru>
2025-05-20 21:24:14 +0300

    add random questions

--- IntelectualTestConsoleApp/Program.cs
+++ IntelectualTestConsoleApp/Program.cs
@@ -1,11 +1,13 @@
-﻿string[] questions = new string[5];
+﻿int arraySize = 5;
+
+string[] questions = new string[arraySize];
 questions[0] = "Сколько будет: 2 + 2 * 2?";
 questions[1] = "Бревно нужно распилить на 10 частей, сколько нужно сделать распилов?";
 questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
 questions[3] = "Укол делают каждые полчаса, сколько минут для трех уколов?";
 questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
 
-int[] answers = new int[5];
+int[] answers = new int[arraySize];
 answers[0] = 6;
 answers[1] = 9;
 answers[2] = 25;
@@ -14,17 +16,20 @@ answers[4] = 2;
 
 int rightAnswersCount = 0;
 
+Random random = new Random();
+
 for (int i = 0; i < questions.Length; i++)
 {
+    int randomIndex = random.Next(0, arraySize);
     int questionNumber = i + 1;
 
-    Console.WriteLine($"Вопрос №{questionNumber}:\n{questions[i]}\n");
+    Console.WriteLine($"Вопрос №{questionNumber}:\n{questions[randomIndex]}\n");
 
     int userAnswer = Convert.ToInt32(Console.ReadLine());
 
     Console.WriteLine();
 
-    int rightAnswer = answers[i];
+    int rightAnswer = answers[randomIndex];
 
     if (userAnswer == rightAnswer)
     {
@@ -32,7 +37,7 @@ for (int i = 0; i < questions.Length; i++)
     }
 }
 
-string[] diagnoses = new string[6];
+string[] diagnoses = new string[arraySize + 1];
 diagnoses[0] = "Идиот";
 diagnoses[1] = "Кретин";
 diagnoses[2] = "Дурак";
@@ -42,4 +47,3 @@ diagnoses[5] = "Гений";
 
 Console.WriteLine($"Вы ответили правильно на {rightAnswersCount} из {questions.Length} вопросов.");
 Console.WriteLine($"Поздравляю, Вы - {diagnoses[rightAnswersCount]}!");
-
