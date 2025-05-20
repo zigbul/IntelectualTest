eb2ac598f084f2ab5a98228b7b37e01122c676c7
Zigbul <parfenov.georgiy@bk.ru>
2025-05-20 21:56:49 +0300

    fix questions repeat

--- IntelectualTestConsoleApp/Program.cs
+++ IntelectualTestConsoleApp/Program.cs
@@ -1,43 +1,42 @@
-﻿int arraySize = 5;
+﻿var questionsAndAnswers = new List<(string question, int answer)>()
+{
+    ("Сколько будет: 2 + 2 * 2?", 6),
+    ("Бревно нужно распилить на 10 частей, сколько нужно сделать распилов?", 9),
+    ("На двух руках 10 пальцев. Сколько пальцев на 5 руках?", 25),
+    ("Укол делают каждые полчаса, сколько минут для трех уколов?", 60),
+    ("Пять свечей горело, две потухли. Сколько свечей осталось?", 2)
+};
 
-string[] questions = new string[arraySize];
-questions[0] = "Сколько будет: 2 + 2 * 2?";
-questions[1] = "Бревно нужно распилить на 10 частей, сколько нужно сделать распилов?";
-questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
-questions[3] = "Укол делают каждые полчаса, сколько минут для трех уколов?";
-questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
-
-int[] answers = new int[arraySize];
-answers[0] = 6;
-answers[1] = 9;
-answers[2] = 25;
-answers[3] = 60;
-answers[4] = 2;
-
+int questionsCount = questionsAndAnswers.Count;
 int rightAnswersCount = 0;
+int questionNumber = 1;
 
 Random random = new Random();
 
-for (int i = 0; i < questions.Length; i++)
+while (questionsAndAnswers.Count > 0)
 {
-    int randomIndex = random.Next(0, arraySize);
-    int questionNumber = i + 1;
+    int randomIndex = random.Next(0, questionsAndAnswers.Count);
+    
+    var (question, answer) = questionsAndAnswers[randomIndex];
 
-    Console.WriteLine($"Вопрос №{questionNumber}:\n{questions[randomIndex]}\n");
+    Console.WriteLine($"Вопрос №{questionNumber}:\n{question}\n");
 
     int userAnswer = Convert.ToInt32(Console.ReadLine());
 
     Console.WriteLine();
 
-    int rightAnswer = answers[randomIndex];
+    int rightAnswer = answer;
 
     if (userAnswer == rightAnswer)
     {
         rightAnswersCount++;
     }
+
+    questionNumber++;
+    questionsAndAnswers.RemoveAt(randomIndex);
 }
 
-string[] diagnoses = new string[arraySize + 1];
+string[] diagnoses = new string[6];
 diagnoses[0] = "Идиот";
 diagnoses[1] = "Кретин";
 diagnoses[2] = "Дурак";
@@ -45,5 +44,5 @@ diagnoses[3] = "Нормальный";
 diagnoses[4] = "Талант";
 diagnoses[5] = "Гений";
 
-Console.WriteLine($"Вы ответили правильно на {rightAnswersCount} из {questions.Length} вопросов.");
+Console.WriteLine($"Вы ответили правильно на {rightAnswersCount} из {questionsCount} вопросов.");
 Console.WriteLine($"Поздравляю, Вы - {diagnoses[rightAnswersCount]}!");
